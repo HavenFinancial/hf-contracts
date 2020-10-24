7e2c1341f24181fbacd386fdc566833fdcb1af65
m.fletcher <m.fletcher@nchain.com>
2020-10-24 18:45:36 +0100

    Added ERC1404 and whitelist contracts

--- asset-factory/src/AssetFactory.sol
+++ asset-factory/src/AssetFactory.sol
@@ -1,9 +1,10 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.4;
 
-contract Test {
-    event Deposit(address indexed _from, bytes32 indexed _id, uint _value);
-    function deposit(bytes32 _id) public payable {
-        emit Deposit(msg.sender, _id, msg.value);
+contract AssetFactory {
+
+    constructor() public {
+
     }
+
 }
