503fd2508b2cb83712f5c61dd6082ef496c9ad22
MikeR808 <mike_richards@hotmail.co.uk>
2020-11-16 19:41:18 +0530

    Added slack integration

--- auto-deploy/auto_deploy.py
+++ auto-deploy/auto_deploy.py
@@ -1,10 +1,13 @@
 from web3 import Web3
 import json
+import requests
 
 def deploy_contract(node, private_key, contract, args):
     w3 = node.w3
     try:
         with open(contract) as f:
+            token = 'xoxb-1439997501442-1504086815714-lZoWyzAufclMkfiMIElUlVSM'
+            url = "https://slack.com/api/chat.postMessage"
             file_content = f.read()
             compiled_sol = json.loads(file_content)
             sender_account = w3.eth.account.privateKeyToAccount(private_key)
@@ -16,7 +19,19 @@ def deploy_contract(node, private_key, contract, args):
             signed_tx = w3.eth.account.signTransaction(transaction, sender_account.privateKey)
             tx_id = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
             tx_receipt = w3.eth.waitForTransactionReceipt(tx_id)
-            print(f'Contract deployed to {tx_receipt.contractAddress}')
+            print(f'New {contract} has been deployed to {tx_receipt.contractAddress}')
+            headers = {
+                "Content-type": "application/json; charset=UTF-8",
+                "Authorization": f'Bearer {token}'
+            }
+            params = {
+                "channel": "#alerts",
+                "text": f'New {contract.split(".")[0]} contract has been deployed to <https://ropsten.etherscan.io/address/{tx_receipt.contractAddress}|{tx_receipt.contractAddress}>'
+            }
+
+            response = requests.post(url, headers=headers, json=params)
+            print(response.status_code)
+            print(response.content)
 
     except Exception as e:
         print("Failed to deploy contract")
